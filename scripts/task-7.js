22598dc525ee05abd2f555465be97c422b78c950
Nataliia333 <n.o.prykhodko@gmail.com>
2020-11-15 23:02:06 +0200

    task-7

--- scripts/task-7.js
+++ scripts/task-7.js
@@ -8,7 +8,6 @@ const input = document.querySelector('#font-size-control');
 const span = document.querySelector('#text');
 
 input.addEventListener('input', onInputChange);
-
 function onInputChange() {
   span.style.fontSize = `${input.value}px`;
 }
