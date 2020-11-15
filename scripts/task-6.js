4e9d9138ec2a787dc374c34a3326365d01c94ec0
Nataliia333 <n.o.prykhodko@gmail.com>
2020-11-15 22:17:41 +0200

    task-6

--- scripts/task-6.js
+++ scripts/task-6.js
@@ -5,3 +5,14 @@
 Если введено подходящее количество, то border инпута становится зеленым, 
 если неправильное - красным.
 Для добавления стилей, используй CSS-классы valid и invalid.*/
+const input = document.querySelector('#validation-input');
+const inputAction = input.dataset.length;
+
+input.addEventListener('input', event => {
+  if (event.target.value.length === Number(inputAction)) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else {
+    input.classList.add('invalid');
+  }
+});
